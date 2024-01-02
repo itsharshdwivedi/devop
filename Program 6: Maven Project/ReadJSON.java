12bdd47979627f3e169682071d69b8bcf50546dc
randomplayer2002 <amith.hathwar02@gmail.com>
2024-01-02 10:51:20 +0530

    Done

---
+++ Program 6: Maven Project/ReadJSON.java
@@ -0,0 +1,28 @@
+package ReadJson.ReadJson;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+
+public class ReadJSON {
+
+	public static void main(String args[]) throws IOException, ParseException {
+
+		JSONParser jsonparser = new JSONParser();
+		FileReader reader = new FileReader(".\\json\\student.json");
+		Object obj = jsonparser.parse(reader);
+		JSONObject studentobj = (JSONObject)obj;
+		String fname = (String)studentobj.get("firstname");
+		String lname = (String)studentobj.get("lastname");
+		System.out.println("firstname"+fname);
+		System.out.println("lastname"+lname);
+
+
+	}
+
+}
